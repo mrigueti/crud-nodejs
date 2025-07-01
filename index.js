37d38f48c82a6b06c7226e241ee1c99b381ad7ae
Maykel <maykelrigueti93@gmail.com>
2025-07-01 10:42:46 -0300

    created POST methode

--- index.js
+++ index.js
@@ -9,18 +9,26 @@ process.env.MONGO_DB;
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+
 const connectdb = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_DB);
-        console.log("Conectado ao MongoDB");
-    } catch (error) {
-        console.log("Erro ao conectar ao MongoDB", error);
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_DB);
+    console.log("Conectado ao MongoDB");
+  } catch (error) {
+    console.log("Erro ao conectar ao MongoDB", error);
+  }
 };
 connectdb();
 
-app.post("/vendas", async (req, res) => (
-    res.send("")
-))
+app.post("/vendas", async (req, res) => {
+  try {
+    const nova_venda_mensal = await VendasSchema.create(req.body);
+    res.json(nova_venda_mensal);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Erro ao criar venda" });
+  }
+});
 
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}`));
